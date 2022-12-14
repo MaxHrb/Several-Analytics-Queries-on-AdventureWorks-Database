b12c486d27cbda8ab5a751cc98b49c439fe743d7
MaxHrb <97131196+MaxHrb@users.noreply.github.com>
2022-12-14 21:54:23 +0100

    Update CTEs_Example_MH.sql

--- CTEs_Example_MH.sql
+++ CTEs_Example_MH.sql
@@ -1,9 +1,4 @@
-/** 
---> Identify the top 10 sales orders per month
---> Aggregate these into a sum total, by month
---> Compare each month´s total to the previous months´, on the same row
-
-**/
+/**  CTEs **/
 
 
 SELECT
