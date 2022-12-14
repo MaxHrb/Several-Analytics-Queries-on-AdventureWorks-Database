925b9a0f599af687bbde3b21c942affc42f77be3
MaxHrb <97131196+MaxHrb@users.noreply.github.com>
2022-12-14 21:53:43 +0100

    Update CorrelatedSubqueries_MH.sql

--- CorrelatedSubqueries_MH.sql
+++ CorrelatedSubqueries_MH.sql
@@ -23,41 +23,8 @@ SELECT
 
   FROM AdventureWorks2019.Sales.SalesOrderHeader A
 
- /** Exercise 1
 
-
-Write a query that outputs all records from the Purchasing.PurchaseOrderHeader table. Include the following columns from the table:
-
-PurchaseOrderID
-
-VendorID
-
-OrderDate
-
-TotalDue
-
-Add a derived column called NonRejectedItems which returns, for each purchase order ID in the query output, the number of line items from the Purchasing.PurchaseOrderDetail table which did not have any rejections (i.e., RejectedQty = 0). Use a correlated subquery to do this.
-
-
-
-Exercise 2
-
-
-Modify your query to include a second derived field called MostExpensiveItem.
-
-This field should return, for each purchase order ID, the UnitPrice of the most expensive item for that order in the Purchasing.PurchaseOrderDetail table.
-
-Use a correlated subquery to do this as well.
-
-
-
-Hint: Think of the most appropriate aggregate function to use in the correlated subquery for this scenario.
-
-**/
-
-
-
-  --Exercise 1 Solution:
+  --Nr1
   
   SELECT * FROM AdventureWorks2019.Purchasing.PurchaseOrderHeader
 
@@ -79,6 +46,7 @@ Hint: Think of the most appropriate aggregate function to use in the correlated 
 
   FROM AdventureWorks2019.Purchasing.PurchaseOrderHeader A
 
+Nr1.1
 
   SELECT 
         PurchaseOrderID
